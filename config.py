af7dc146df94b478db434da41d39f1db45730083
Diego Rojas <devup2332@gmail.com>
2023-09-13 18:25:19 -0500

    master: Edited spaces

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 
-
 mod = "mod4"
 terminal = "alacritty"
 
